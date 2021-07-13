7b891139975c750040229343bb340b76301d0056
victor131999 <tovir.gu.hu@hotmail.com>
2021-07-12 23:53:23 -0500

    Logo de inicio y paginacion

--- tapicarpas/resources/views/responsable/create.blade.php
+++ tapicarpas/resources/views/responsable/create.blade.php
@@ -2,6 +2,6 @@
 <form action="{{url('/responsable')}}" method="post" enctype="multipart/form-data">
 @csrf
 
-    @include('responsable.form',['modo'=>'Crear'])
+    @include('responsable.form',['modo'=>'Crear '])
 
 </form>
