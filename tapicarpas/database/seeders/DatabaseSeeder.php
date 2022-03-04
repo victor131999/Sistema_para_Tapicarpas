b69ccb6a793c0afa57e1617894362e54c57403e9
victor131999 <tovir.gu.hu@hotmail.com>
2022-03-03 22:40:15 -0500

    usuario por defecto

--- tapicarpas/database/seeders/DatabaseSeeder.php
+++ tapicarpas/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +14,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name'  => 'ADMIN',
+            'email'     => 'administrador@admin.com',
+            'password'  => bcrypt('admin'),
+            'current_team_id' => 1,
+        ]);
+
+        DB::table('teams')->insert([
+            'user_id'  => 1,
+            'name'     => "admin's Team",
+            'personal_team'  => 1,
+        ]);
     }
 }
